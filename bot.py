a33665a84fb14d1c68dab07c60f8e600adfdf2eb
radebeneo <radebeneo17@gmail.com>
2026-04-07 14:11:34 +0200

    initial project setup.

---
+++ bot.py
@@ -0,0 +1,5 @@
+from lumibot.brokers import Alpaca
+from lumibot.backtesting import YahooDataBacktesting
+from lumibot.strategies.strategy import Strategy
+from lumibot.traders import Trader
+from datetime import datetime
